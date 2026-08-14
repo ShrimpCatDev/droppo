1fe1ff21cfe980505b0483f46aa22bcbc79807cf
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-08-14 15:58:34 -0500

    addeing a lil polish to the player's movement

--- main.lua
+++ main.lua
@@ -11,6 +11,7 @@ function initSprite(img,size)
 end
 
 function love.load()
+    timer=require("lib/hump/timer")
     font = require("assets/font/skull")
     lg.setFont(font)
     shove.createLayer("game")
@@ -24,13 +25,16 @@ function love.load()
         lg.draw(sheet,quads[tile+1],x,y,...)
     end
 
-    pl={x=0,y=0,tile=72}
+    pl={x=0,y=0,tile=72,move=true,dz=0}
+    pl.dx=pl.x*size
+    pl.dy=pl.y*size
 end
 
 function love.update(dt)
+    timer.update(dt)
     input:update()
     local boo=input:pressed("up") or input:pressed("down") or input:pressed("left") or input:pressed("right")
-    if boo then
+    if boo and pl.move then
         local px,py=pl.x,pl.y
         if input:pressed("up") then
             pl.y=pl.y-1
@@ -57,7 +61,17 @@ function love.update(dt)
                 pl.y=py
             else
                 map:setLayerTile("terrain",px+1,py+1,0)
+                pl.move=false
+                timer.tween(0.15,pl,{dx=pl.x*size,dy=pl.y*size},"out-cubic",function()
+                    pl.move=true
+                end)
+                timer.tween(0.15/2,pl,{dz=-3},"out-cubic",function()
+                    timer.tween(0.15/2,pl,{dz=0},"in-cubic",function()
+                    
+                    end)
+                end)
             end
+            
         end
     end
 end 
@@ -66,8 +80,8 @@ function love.draw()
     beginDraw()
         map:draw()
         lg.setColor(0,0,0,0.4)
-        lg.rectangle("fill",pl.x*size,pl.y*size,8,8)
+        lg.rectangle("fill",pl.dx,pl.dy,8,8)
         lg.setColor(1,1,1)
-        spr(pl.tile,pl.x*size,pl.y*size-2)
+        spr(pl.tile,pl.dx,pl.dy-2+pl.dz)
     endDraw()
 end
